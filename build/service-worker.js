5d6d64650ab5807f888d244d9929546cad91301c
JohnnyBcuche <kisivan92@gmail.com>
2019-03-12 21:35:18 +0100

    fixed build folder

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Cards-game/precache-manifest.102f310dc5ad51baac17845565de99bd.js"
+  "/Calculator/precache-manifest.0512afd2aeb9083451db64a2239152a8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Cards-game/index.html", {
+workbox.routing.registerNavigationRoute("/Calculator/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
