52fcc25de2edf47ff1c8cb50614cc3319c3d94f0
JohnnyBcuche <kisivan92@gmail.com>
2019-03-13 12:12:37 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Calculator/precache-manifest.0512afd2aeb9083451db64a2239152a8.js"
+  "/aaaaaaaa/precache-manifest.d1506a3b43fac5ac8be908a2e7f825c3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Calculator/index.html", {
+workbox.routing.registerNavigationRoute("/aaaaaaaa/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
